994334def365e5e66ccc2e77f5464d50ee9f811d
Haryadi Nugraha Putra <h.nugraha.p@gmail.com>
2026-08-07 11:54:39 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+const a = new Date(2026, 7, 6);
+
+const b = new Date('2026-08-06');
+
+console.log('A');
+console.log(a);
+console.log(a.toISOString());
+
+console.log('');
+
+console.log('B');
+console.log(b);
+console.log(b.toISOString());
