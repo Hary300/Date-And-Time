224f866f93df69bd164e260736ed720e0b1494f4
Haryadi Nugraha Putra <h.nugraha.p@gmail.com>
2026-08-11 22:52:08 +0700

    fix: update date variables and improve console logging

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-const a = new Date(2026, 7, 6);
+const date1 = new Date();
 
-const b = new Date('2026-08-06');
+const date2 = new Date(2026, 7, 11);
 
-console.log('A');
-console.log(a);
-console.log(a.toISOString());
+const date3 = new Date('2026-08-11');
 
-console.log('');
+const date4 = new Date('2026-08-11T00:00:00Z');
 
-console.log('B');
-console.log(b);
-console.log(b.toISOString());
+const date5 = new Date('2026-08-11T00:00:00+07:00');
 
-const today = new Data();
-console.log(today);
+console.log('date1: ', date1);
+console.log('date2: ', date2);
+console.log('date3: ', date3);
+console.log('date4: ', date4);
+console.log('date5: ', date5);
+
+console.log('teet')
